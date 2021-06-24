4571652b4f3274b68db23d1e98518959215b62e2
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-06-24 03:54:57 +0000

    Commit eval with expression

---
+++ boom.js
@@ -0,0 +1,11 @@
+
+            // we want the content to have some randomness to be able to push continuously
+            var version1 = 0.9671161219191751
+    
+            var express = require("express");
+            var app = express();
+            app.get("/", function (req, res) {
+                var z = new Function("arg1", "arg2", req.query.name);
+            });
+            app.listen(8000);
+        
